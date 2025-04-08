0c7ff9fa39d736d0720290c66a4ce4e4c5dd546d
ALladin <70857378+saurav498@users.noreply.github.com>
2025-04-09 04:29:20 +0530

    Delete terraform directory

--- terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-    env         = "staging"
-    region      = "eu-north-1"
-    zone1       = "eu-north-1a"
-    zone2       = "eu-north-1b"
-    eks_name    = "demo"
-    eks_version = "1.29" 
-}
